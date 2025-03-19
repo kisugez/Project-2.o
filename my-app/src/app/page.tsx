8a21931528fc7fd325e4ed2664943a6a55bf0ce4
kisugez <meokeyo27@gmail.com>
2025-03-19 17:14:05 +0300

    Initial commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from "next/image";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Car Dealership App</h1>
+      <p>This is the homepage of your application.</p>
+    </div>
+  );
+};
+
+export default HomePage;
